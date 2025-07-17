10ccc011f4bfa7ca7f499c818529b936b48f4f36
Erica Olliges <ejolli01@gmail.com>
2025-07-16 17:47:10 -0700

    remove table from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -128,38 +128,7 @@ export default function Home() {
             </div>
           </div>
         </form>
-
         <div className="flex flex-col mb-16 gap-6">{advocateResults}</div>
-        <table>
-          <thead>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </thead>
-          <tbody>
-            {advocates.map((advocate) => {
-              return (
-                <tr>
-                  <td>{advocate.firstName}</td>
-                  <td>{advocate.lastName}</td>
-                  <td>{advocate.city}</td>
-                  <td>{advocate.degree}</td>
-                  <td>
-                    {advocate.specialties.map((s) => (
-                      <div>{s}</div>
-                    ))}
-                  </td>
-                  <td>{advocate.yearsOfExperience}</td>
-                  <td>{advocate.phoneNumber}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
         <div className="flex flex-row gap-4 mb-24 justify-end">
           {cursor > 0 && <Button onClick={loadPreviousAdvocates}>Back</Button>}
           {nextCursor < count && (
