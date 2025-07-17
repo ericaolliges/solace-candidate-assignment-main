683f7a9e13ebf952ad75282d26e5c6ba417bdfc1
Erica Olliges <ejolli01@gmail.com>
2025-07-16 17:59:29 -0700

    add class name to named text

--- src/app/components/atoms/NamedText.tsx
+++ src/app/components/atoms/NamedText.tsx
@@ -3,11 +3,12 @@ import React, { FC, ReactNode } from "react";
 interface NamedTextProps {
   children: ReactNode;
   name: string;
+  className?: string;
 }
 
-const NamedText: FC<NamedTextProps> = ({ children, name }) => {
+const NamedText: FC<NamedTextProps> = ({ children, className, name }) => {
   return (
-    <p className="text-base">
+    <p className={`text-base ${className}`}>
       <span className="font-bold">{name}: </span>
       {children}
     </p>
