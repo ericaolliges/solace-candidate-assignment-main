a5aa4a5327edbadba8dae53864a95becbab77467
Erica Olliges <ejolli01@gmail.com>
2025-07-24 01:28:28 -0700

    update heading component with separate text size

--- src/app/components/atoms/Heading.tsx
+++ src/app/components/atoms/Heading.tsx
@@ -1,24 +1,47 @@
 import React, { FC, ReactNode } from "react";
 
 interface HeadingProps {
-  level: 1 | 2 | 3 | 4 | 5 | 6;
   children: ReactNode;
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+  textSize?: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
-const Heading: FC<HeadingProps> = ({ children, level }) => {
+const Heading: FC<HeadingProps> = ({ children, level, textSize }) => {
+  let headingClassName: string;
+  switch (textSize ?? level) {
+    case 1:
+      headingClassName = "text-4xl sm:text-5xl";
+      break;
+    case 2:
+      headingClassName = "text-3xl sm:text-4xl";
+      break;
+    case 3:
+      headingClassName = "text-2xl sm:text-3xl";
+      break;
+    case 4:
+      headingClassName = "text-xl sm:text-2xl";
+      break;
+    case 5:
+      headingClassName = "text-lg sm:text-xl";
+      break;
+    case 6:
+      headingClassName = "text-base sm:text-lg";
+      break;
+  }
+
   switch (level) {
     case 1:
-      return <h1 className="text-4xl sm:text-5xl">{children}</h1>;
+      return <h1 className={headingClassName}>{children}</h1>;
     case 2:
-      return <h2 className="text-2xl sm:text-3xl">{children}</h2>;
+      return <h2 className={headingClassName}>{children}</h2>;
     case 3:
-      return <h3 className="text-2xl sm:text-3xl">{children}</h3>;
+      return <h3 className={headingClassName}>{children}</h3>;
     case 4:
-      return <h4 className="text-xl sm:text-2xl">{children}</h4>;
+      return <h4 className={headingClassName}>{children}</h4>;
     case 5:
-      return <h5 className="text-lg sm:text-xl">{children}</h5>;
+      return <h5 className={headingClassName}>{children}</h5>;
     case 6:
-      return <h6 className="text-base sm:text-lg">{children}</h6>;
+      return <h6 className={headingClassName}>{children}</h6>;
   }
 };
 
